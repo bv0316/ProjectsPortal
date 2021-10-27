fcaabcdc659bd2a80655d861c95ecf2ee3406f01
Bharath Vallabhaneni <BV0316@dexcom.com>
2021-10-26 17:19:22 -0700

    Made changes to ul

--- PortalSearch/urls.py
+++ PortalSearch/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name= "reset_password_complete.html"), name= 'password_reset_complete'),
 
 ]
-#Url here
+#Url patterns
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
